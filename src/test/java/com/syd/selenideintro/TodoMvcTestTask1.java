6850f8f6899165b21806a36e38c293d9968f619c
Evgeny <j7.odessa@gmail.com>
2020-07-04 11:05:07 +0300

    some changes

--- src/test/java/com/syd/selenideintro/TodoMvcTestTask1.java
+++ src/test/java/com/syd/selenideintro/TodoMvcTestTask1.java
@@ -10,19 +10,10 @@ import static com.codeborne.selenide.Selenide.*;
 public class TodoMvcTestTask1 {
     @Test
     void completeTask() {
-        open("http://todomvc.com/examples/emberjs/");
-
-        element("#new-todo").setValue("a").pressEnter();
-        element("#new-todo").setValue("b").pressEnter();
-        element("#new-todo").setValue("c").pressEnter();
-
-
-        elements("#todo-list>li").shouldHave(exactTexts("a", "b", "c"));
-        element("#ember11>div>.toggle").click();
-        element("#ember11").shouldHave(cssClass("completed")).shouldHave(exactText("b"));
-        element("#ember6").shouldHave(not(cssClass("completed"))).shouldHave(exactText("a"));
-        element("#ember13").shouldHave(not(cssClass("completed"))).shouldHave(exactText("c"));
         
+
+
+
     }
 
 }
